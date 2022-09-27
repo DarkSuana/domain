d64b3577b92a1bdc80558f056b86e7c8dccb915f
DarkSuana <Isstudents078@gmail.com>
2022-09-27 11:58:47 +0300

    Готово

--- freeipa.sh
+++ freeipa.sh
@@ -1,1 +1,11 @@
-
+#!/bin/bash
+apt-get update && apt-get upgrade -y
+echo "Enter your domain (default=test.local): "
+read $DOMAIN
+echo "Enter hostname of your server (default=ipa):"
+read $HOSTNAME
+hostnamectl set-hostname $HOSTNAME+$DOMAIN
+echo "Enter youк local ip: "
+read $IP
+echo -e "$IP $HOSTNAME+$DOMAIN  $HOSTNAME" | tee -a /etc/hosts
+apt-get install rng-tools -y
