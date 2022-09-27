630ef279990aff49ea2ea2fd9e4aaa98939da274
DarkSuana <Isstudents078@gmail.com>
2022-09-27 12:39:29 +0300

    gfh

--- freeipa.sh
+++ freeipa.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-DOMAIN="test.local"
-HOSTNAME="ipa"
-echo "Enter your domain (default=test.local): "
+echo "Enter your domain: "
 read DOMAIN
-echo "Enter hostname of your server (default=ipa):"
+echo "Enter hostname of your server:"
 read HOSTNAME
 hostnamectl set-hostname $HOSTNAME$DOMAIN
 echo "Enter youк local ip: "
 read IP
-echo "$IP    $HOSTNAME$DOMAIN   $HOSTNAME" | tee -a /etc/hosts
+echo "$IP    $HOSTNAME.$DOMAIN   $HOSTNAME" | tee -a /etc/hosts
 apt-get update && apt-get upgrade -y
 apt-get install rng-tools -y
+echo "HRNGDEVICE=/dev/urandom" | tee -a /etc/default/rng-tools
+systemctl enable rng-tools
+systemctl start rng-tools
+apt-get install freeipa-server
+ipa-server-install
