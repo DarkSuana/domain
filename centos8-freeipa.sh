d99eb77134417664e2ec3f5d14f7e46842c53f69
DarkSuana <Isstudents078@gmail.com>
2022-09-27 15:32:24 +0300

    gfgffg

--- centos8-freeipa.sh
+++ centos8-freeipa.sh
@@ -7,7 +7,7 @@ hostnamectl set-hostname $HOSTNAME$DOMAIN
 echo "Enter youк local ip: "
 read IP
 dnf update -y
-timedatectl set-timezone Europe-Moscow
+timedatectl set-timezone Europe/Moscow
 dnf install chorny
 systemctl enable chronyd --now
 echo "$IP    $HOSTNAME.$DOMAIN   $HOSTNAME" | tee -a /etc/hosts
@@ -15,6 +15,6 @@ firewall-cmd --permanent --add-port=53/{tcp,udp} --add-port={80,443}/tcp --add-p
 firewall-cmd --reload
 setenforce 0
 sed -i 's/^SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
-dnf install @idm:DL1
-dnf install ipa-server
+dnf install @idm:DL1 -y 
+dnf install ipa-server -y 
 ipa-server-install
